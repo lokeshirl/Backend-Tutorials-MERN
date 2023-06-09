a2446ae5d99a876e403592a6ce6c5bc1aa2741b6
lokeshirl <lokeshkumarjha07+github@gmail.com>
2023-06-10 02:40:40 +0530

    backend-5 -- mongo-basics-and-atlas revision done✅

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const productRouter = require('./routes/product');
 const usersRouter = require('./routes/users');
 
+console.log('env', process.env.DB_PASSWORD);
+
 const server = express();
 
 // morgan - used for logger
 server.use(morgan('default'));
-server.use(express.static('public'));
+server.use(express.static(process.env.PUBLIC_DIR));
 server.use(express.json());
 server.use('/products', productRouter.router);
 server.use('/users', usersRouter.router);
 
-server.listen(8080, () => {
+server.listen(process.env.PORT, () => {
   console.log('server started');
 });
